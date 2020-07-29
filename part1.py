0db40de7dfd004f0cb67e1988a40413ce25ea678
Parham Taheri | پرهام طاهری <62795984+parhamoyan@users.noreply.github.com>
2020-07-29 19:47:33 +0200

    Update part1.py

--- part1.py
+++ part1.py
@@ -1,6 +1,6 @@
 import os
 
-def display(board):
+def display():
     print(*[" "*4+"{} | {} | {}\n".format(*[symbols[index] for index in board[i]]) for i in range(3)], sep=" "*3+"-"*11+"\n")
 
 def play(i, j):
@@ -9,7 +9,7 @@ def play(i, j):
 def choiceValidity(i, j):
     return 0<=i<=2 and 0<=j<=2 and board[i][j]==0
 
-def checkWin(board):
+def checkWin():
     all_lists = [column for column in board] \
             + [[board[i][row] for i in range(3)] for row in range(3)] \
             + [[board[i][i] for i in range(3)]] + [[board[i][2-i] for i in range(3)]]
@@ -30,16 +30,17 @@ if __name__ == "__main__":
     board = [[0 for i in range(3)] for j in range(3)]
     player = 1
     symbols = [" ", "X", "O"]
-    display(board)
-    while not checkWin(board):
+    display()
+    while not checkWin():
         i = int(input("Enter i : "))
         j = int(input("Enter j : "))
         if choiceValidity(i, j):
             play(i, j)
         player = 2 - (player+1)%2
         clear()
-        display(board)
-    if checkWin(board)==3:
+        display()
+    messages = ["Player X has won !", "Player O has won !", "Game Draw"]
+    if checkWin()==3:
         print("Game Draw")
     else:
-        print("Player {} has won !".format(["X", "O"][checkWin(board)-1]))
+        print("Player {} has won !".format(["X", "O"][checkWin()-1]))
