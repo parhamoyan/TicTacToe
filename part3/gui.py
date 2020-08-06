567fa904961f3d880dedc50153b23cf8d7a9e7c7
Parham Taheri | پرهام طاهری <parisonoyan@yahoo.com>
2020-08-06 19:05:47 +0200

    Update gui.py

--- part3/gui.py
+++ part3/gui.py
@@ -17,6 +17,7 @@ class App(QWidget):
         for i in range(3):
             for j in range(3):
                 self.labels[i][j].setGeometry(30+220*i, 20+220*j, 200, 200)
+        self.messages = ["Player X has won !", "Player O has won !", "Game Draw"]
         self.initUI()
         
     def initUI(self):
@@ -33,21 +34,20 @@ class App(QWidget):
         painter.drawRect(20, 220, 640, 20)
         painter.drawRect(20, 440, 640, 20)
         
-    
     def mousePressEvent(self, event):
         i, j = event.pos().x()//220, event.pos().y()//220
-        if self.game.choiceValidity(i, j) and self.game.checkWin()==0:
+        if self.game.choiceValidity(i, j) and not self.game.checkWin():
             self.game.play(i, j)
             pixmap = QPixmap("{}_PLAYER.png".format(["X", "O"][self.game.getPlayer()-1])).scaled(180, 180)
             self.labels[i][j].setPixmap(pixmap)
+            if self.game.checkWin():
+                buttonReply = QMessageBox.question(self, 'Message', self.messages[self.game.getPlayer()], QMessageBox.Ok)
+                if buttonReply == QMessageBox.Yes:
+                    print('Yes clicked.')
+                else:
+                    print('No clicked.')
             self.game.changePlayer()
-            buttonReply = QMessageBox.question(self, 'Message', "win", QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
-            if buttonReply == QMessageBox.Yes:
-                print('Yes clicked.')
-            else:
-                print('No clicked.')
-
-    
+            
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
