e475a1f2f1d523b29b11c35d78106e5d9bd88023
Parham Taheri | پرهام طاهری <parisonoyan@yahoo.com>
2020-10-11 16:20:20 +0200

    Update TicTacToe.sh

--- Bash/TicTacToe.sh
+++ Bash/TicTacToe.sh
@@ -1,31 +1,66 @@
 #! /bin/bash
 
-board=(" " " " " " " " " " " " " " " " " ")
-symbols=("X" "O")
+board=(0 0 0 0 0 0 0 0 0)
+symbols=(" " "X" "O")
 player=1
+gameStatus=0
 
 display(){
-    echo "  ${board[0]} | ${board[1]} | ${board[2]}"
+    echo
+    echo "  ${symbols[board[0]]} | ${symbols[board[1]]} | ${symbols[board[2]]}"
     echo "-------------"
-    echo "  ${board[3]} | ${board[4]} | ${board[5]}"
+    echo "  ${symbols[board[3]]} | ${symbols[board[4]]} | ${symbols[board[5]]}"
     echo "-------------"
-    echo "  ${board[6]} | ${board[7]} | ${board[8]}"
+    echo "  ${symbols[board[6]]} | ${symbols[board[7]]} | ${symbols[board[8]]}"
+    echo
 }
 
 
+checkCase(){
+    if [ ${board[$1]} != 0 ] && [ ${board[$1]} == ${board[$2]} ] && [ ${board[$2]} == ${board[$3]} ]
+    then
+        gameStatus=${board[$1]}
+    fi
+}
+
+checkWin(){
+    # check all rows
+    i=0
+    while [ ${gameStatus} == 0 ] && [ ${i} -lt 3 ]
+    do
+        checkCase $(( 3*i )) $(( 3*i+1 )) $(( 3*i+2 ))
+        i=$((i+1))
+    done
+    # check all columns
+    i=0
+    while [ ${gameStatus} == 0 ] && [ ${i} -lt 3 ]
+    do
+        checkCase $(( i )) $(( i+3 )) $(( i+6 ))
+        i=$((i+1))
+    done
+    # check all diagonals
+    checkCase 0 4 8
+    checkCase 2 4 6
+}
+
+clear
+display
 while true
 do
-    clear
-    display
     echo "Enter i : "
     read i
     echo "Enter j : "
     read j
-    echo ${board[i*3+j]}
-    if [[ "${board[i*3+j]}" == " " ]]
+    if [[ ${board[i*3+j]} == 0 ]]
     then
-        board[i*3+j]=${symbols[player-1]}
+        board[i*3+j]=${player}
         player=$(((player)%2+1))
-        echo "Player = " "${player}"
+    fi
+    clear
+    display
+    checkWin
+    if [ $gameStatus != 0 ]; then
+        echo "Player" "${symbols[gameStatus]}" "has won !"
+        break
     fi
 done
